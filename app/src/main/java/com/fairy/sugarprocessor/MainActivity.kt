b6c06ba685b9df5114fd2b5e3711db323c68cf94
Zacharias <z.hadji02@gmail.com>
2021-01-31 14:15:11 +0200

    fixes

--- app/src/main/java/com/fairy/sugarprocessor/MainActivity.kt
+++ app/src/main/java/com/fairy/sugarprocessor/MainActivity.kt
@@ -3,6 +3,7 @@ package com.fairy.sugarprocessor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
